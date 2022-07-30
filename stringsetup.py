b8889769f58708bb5339eaf3025007f122d18d29
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-07-30 09:07:12 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -6,11 +6,11 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-print("•••   WARUSERBOT  SESSION  GENERATOR   •••")
-print("\nHello!! Welcome to WarBot Session Generator\n")
+print("•••   DANGERCAT  SESSION  GENERATOR   •••")
+print("\nHello!! Welcome to dangercat Session Generator\n")
 okvai = input("Enter 69 to continue: ")
 if okvai == "69":
-    print("Choose the string session type: \n1. WARUSERBOT \n2. Music Bot")
+    print("Choose the string session type: \n1. DANGERCAT \n2. Music Bot")
     library = input("\nYour Choice: ")
     if library == "1":
         print("\nTelethon Session For WarUserBot")
@@ -18,14 +18,14 @@ if okvai == "69":
         API_HASH = input("\nEnter API HASH here: ")
         with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
             print("\nYour Waruserbot Session Is sent in your Telegram Saved Messages.")
-            warbot.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{warbot.session.save()}`")
+            warbot.send_message("me", f"#DANGERCAT #DANGERCAT_SESSION \n\n`{dangercat.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as hellbot:
-            print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
-            warbot.send_message("me", f"#WARUSERBOT_MUSIC #WARUSERBOT_SESSION\n\n`{warbot.export_session_string()}`")
+            print("\nYour dangercat Session Is sent in your Telegram Saved Messages.")
+            dangercat.send_message("me", f"#dangercat #dangercat_SESSION\n\n`{dangercat.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
